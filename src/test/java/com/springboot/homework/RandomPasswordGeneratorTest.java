c58df7977ee9033fadaea7e5dddb97792785f214
6feetlife <6feetlife56@gmail.com>
2025-01-21 16:52:19 +0900

    구본석 test 과제 제출

--- src/test/java/com/springboot/homework/RandomPasswordGeneratorTest.java
+++ src/test/java/com/springboot/homework/RandomPasswordGeneratorTest.java
@@ -1,12 +1,63 @@
 package com.springboot.homework;
 
+import com.springboot.helper.RandomPasswordGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RandomPasswordGeneratorTest {
     @DisplayName("실습 3: 랜덤 패스워드 생성 테스트")
     @Test
     public void generateTest() {
         // TODO 여기에 테스트 케이스를 작성해주세요.
+        int actual = 10;
+        int expected = RandomPasswordGenerator.generate(2,3,3,2).length();
+
+
+        assertEquals(actual,expected);
+    }
+    @DisplayName("생성된 패스워드의 '알파벳 대문자' 개수")
+    @Test
+    public void generateTest2() {
+        String passWord = RandomPasswordGenerator.generate(2,3,3,2);
+
+        int expected = (int)passWord.chars().filter(e -> Character.isUpperCase(e)).count();
+        int acutal = 2;
+
+        assertEquals(expected,acutal);
+    }
+    @DisplayName("생성된 패스워드의 '알파벳 소문자' 개수")
+    @Test
+    public void generateTest3() {
+        String passWord = RandomPasswordGenerator.generate(2,3,3,2);
+
+        int expected = (int)passWord.chars().filter(e -> Character.isLowerCase(e)).count();
+        int acutal = 3;
+
+        assertEquals(expected,acutal);
+    }
+    @DisplayName("생성된 패스워드의 '숫자' 개수")
+    @Test
+    public void generateTest4() {
+        String passWord = RandomPasswordGenerator.generate(2,3,3,2);
+
+        int expected = (int)passWord.chars().filter(e -> Character.isDigit(e)).count();
+        int acutal = 3;
+
+        assertEquals(expected,acutal);
+    }
+    @DisplayName("생성된 패스워드의 '특수문자' 개수")
+    @Test
+    public void generateTest5() {
+        String passWord = RandomPasswordGenerator.generate(2,3,3,2);
+
+        int expected = (int)passWord.chars().filter(e -> !Character.isLetterOrDigit(e)).count();
+        int acutal = 2;
+
+        assertEquals(expected,acutal);
     }
 }
--- src/test/java/com/springboot/homework/StampCalculatorTest.java
+++ src/test/java/com/springboot/homework/StampCalculatorTest.java
@@ -1,18 +1,51 @@
 package com.springboot.homework;
 
+import com.springboot.helper.StampCalculator;
+import com.springboot.order.entity.Order;
+import com.springboot.order.entity.OrderCoffee;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StampCalculatorTest {
     @Test
     @DisplayName("실습1: 스탬프 카운트 계산 단위 테스트")
     public void calculateStampCountTest() {
         // TODO 여기에 테스트 케이스를 작성해주세요.
+
+        int actual = 7;
+        int expected = StampCalculator.calculateStampCount(3,4);
+
+        assertEquals(actual, expected);
+
     }
 
     @Test
     @DisplayName("실습1: 주문 후 누적 스탬프 카운트 계산 단위 테스트")
     public void calculateEarnedStampCountTest(){
         // TODO 여기에 테스트 케이스를 작성해주세요.
+
+        OrderCoffee orderCoffee = new OrderCoffee();
+        orderCoffee.setQuantity(3);
+
+        OrderCoffee orderCoffee2 = new OrderCoffee();
+        orderCoffee2.setQuantity(6);
+
+        List<OrderCoffee> orderCoffees = new ArrayList<>();
+        orderCoffees.add(orderCoffee);
+        orderCoffees.add(orderCoffee2);
+
+        Order order = new Order();
+        order.setOrderCoffees(orderCoffees);
+
+        int actual = StampCalculator.calculateEarnedStampCount(order);
+        int expected = 9;
+
+        assertEquals(expected, actual);
+
     }
 }
